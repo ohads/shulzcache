50e1def9ede2ef0cae6d670cb0c72a62e2087d5a
Ohad Shulz <oshulz@infinipoint.io>
2025-06-28 01:44:09 +0300

    cleanup

--- main.go
+++ main.go
@@ -9,36 +9,21 @@ import (
 	shulzcache "github.com/ohads/shulzcache/shuzlcache"
 )
 
-func a(id int) (string, error) {
+func fetchDataFromRemote(id int) (string, error) {
 	time.Sleep(2 * time.Second)
 	log.Printf("running the actual func for id %d\n", id)
 	return fmt.Sprintf("Result for ID %d", id), nil
 }
 
 func main() {
-
-	b := shulzcache.NewCachedFunction(a)
+	cachedFetch := shulzcache.NewCachedFunction(fetchDataFromRemote)
 
 	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
-			result, err := b(100 + i%2)
-			if err == nil {
-				log.Println("Got:", result)
-			}
-		}(i)
-	}
-
-	wg.Wait()
-
-	time.Sleep(6 * time.Second)
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			result, err := b(100 + i%2)
+			result, err := cachedFetch(100 + i%2)
 			if err == nil {
 				log.Println("Got:", result)
 			}
